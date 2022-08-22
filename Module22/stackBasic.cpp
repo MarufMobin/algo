36dc4eb3c6b9ea5a1c776bb281486e4f7f8b3403
Maruf Mobin <maruf.m@crtvecode.in>
2022-08-22 20:01:52 +0600

    Implementation Of Stack Part 2

--- Module22/stackBasic.cpp
+++ Module22/stackBasic.cpp
@@ -18,27 +18,79 @@ class Stack
 {
     Node *head;
     Node *top;
+    int count = 0;
 
 public:
     // PUSH
-    void push( int val ){
-        
-        Node* newNode = new Node( val );
+    void push(int val)
+    {
 
-        if( head == NULL ){
+        Node *newNode = new Node(val);
+
+        if (head == NULL)
+        {
             head = top = newNode;
+            count++;
             return;
         }
-        
-        top->next = newNode;
-        newNode->prev = top;    
-        top = newNode;
 
+        top->next = newNode;
+        newNode->prev = top;
+        top = newNode;
+        count++;
     }
     // POP
+    int pop()
+    {
+        Node *delNode;
+        delNode = top;
+        int check = -1;
+        // There is No Element in this Stack
+        if (head == NULL)
+        {
+            cout << "Stack Underflow " << endl;
+            return check;
+        }
+        if (top == head) // There is Only One Element
+        {
+            head = top = NULL;
+        }
+        else // There is More then One Element
+        {
+            top = delNode->prev;
+            top->next = NULL;
+        }
+        check = delNode->value;
+        delete delNode;
+         count--;
+        return check;
+    }
     // EMPTY
+
+    bool empty()
+    {
+        if (head == NULL)
+            return true;
+        else
+            return false;
+    }
     // SIZE
+    int size()
+    {
+        return count;
+    }
     // TOP
+    int top()
+    {
+        if (top == NULL)
+        {
+            cout << "Stack Underflow | There is no Element in Top " << endl;
+        }
+        else
+        {
+            return top->value;
+        }
+    }
 };
 
 int main()
