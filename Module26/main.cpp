e6c82385999c5b7000d4e11e25593eef5becf484
Maruf Mobin <maruf.m@crtvecode.in>
2022-08-27 00:09:17 +0600

    queue Generic Form are Well Define and use perfectly all kind of data type

--- Module26/main.cpp
+++ Module26/main.cpp
@@ -1,26 +1,38 @@
-
 #include<bits/stdc++.h>
-#include"MYQUEUE.h"
+#include"MYQUEUETEMP.h"
 using namespace std;
 
+// typedef pair< int , int > mytype;
+typedef pair< char , int > mytype;
+
 int main(){
-    Queue q;
+    // Queue<int> q;
+    Queue<mytype> q;
     int n;
     cin >> n;
     for( int i = 0; i < n; i++ ){
-        int chk;
-        cin >> chk;
-        q.push(chk);
+        char c1;
+        int c2;
+        cin >> c1 >> c2;
+        q.push(make_pair(c1, c2));
+        // int chk;
+        // cin >> chk;
+        // q.push(chk);
     }
     cout << endl << endl;
-    // while( !q.empty() ){
-    //     cout << q.pop() << " ";
+    while( !q.empty() ){
+        mytype chk;
+        chk  = q.pop();
+        cout << chk.first  << " | " << chk.second << endl;
+    }
+
+    // if( !q.empty() ){
+    //     cout << q.Front().first << endl;
+    //     cout << q.Front().second << endl;
     // }
-    if( !q.empty() ){
-        cout << q.Front() << endl;
-    }
-    if( !q.empty() ){
-        cout << q.Back() << endl;
-    }
+    // if( !q.empty() ){
+    //     cout << q.Back().first << endl;
+    //     cout << q.Back().second << endl;
+    // }
     return 0;
 }
