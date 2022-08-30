74733fa151be689c9274ba676b2b090723ba0bc1
Maruf Mobin <mobinh264@gmail.com>
2022-08-30 21:16:21 +0600

    Binary Tree Implementation Write and Code Also Exicuted

--- Module30/binaryTreeImplematation.cpp
+++ Module30/binaryTreeImplematation.cpp
@@ -1,49 +1,101 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-class treeNode{
+class treeNode
+{
 public:
     int data;
-    treeNode* leftChild;
-    treeNode* rightChild;
-    treeNode( int value ){
+    treeNode *leftChild;
+    treeNode *rightChild;
+    treeNode(int value)
+    {
         data = value;
         leftChild = NULL;
         rightChild = NULL;
     }
 };
-void printTree( treeNode* root, int level );
+// Show Print The Tree
+void printTree(treeNode *root, int level);
+// Space Printer also Base on Level
+void spacePrint(int level);
 
-void printTree( treeNode* root, int level ){
-
-}
-int main(){
-    int n;
-    cin >> n;
-    treeNode* allNodes[n];
-    for( int i = 0; i < n; i++ ){
-        allNodes[i]->data = -1;
+void printTree(treeNode *root, int level)
+{
+    if (root == NULL) // When the Tree Empty
+    {
+        return;
+    }
+    if (root->leftChild == NULL && root->rightChild == NULL) // CASE 1
+    {
+        cout << root->data << endl;
+        return;
+    }
+    else // Case 2
+    {
+        cout << endl;
+        spacePrint(level);
+        cout << "Root: " << root->data << endl;
     }
 
-    for( int i = 0; i < n; i++ ){
+    if (root->leftChild != NULL)
+    {
+        spacePrint(level);
+        cout << "Left: ";
+        printTree(root->leftChild, level + 1);
+    }
+
+    if (root->rightChild != NULL)
+    {
+        spacePrint(level);
+        cout << "Right: ";
+        printTree(root->rightChild, level + 1);
+    }
+}
+
+// Space Printer
+void spacePrint(int level)
+{
+    for (int i = 0; i < level; i++)
+    {
+        cout << "   ";
+    }
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    treeNode *allNodes[n];
+    for (int i = 0; i < n; i++)
+    {
+        allNodes[i] = new treeNode(-1);
+    }
+
+    for (int i = 0; i < n; i++)
+    {
         int value, left, right;
         cin >> value >> left >> right;
         allNodes[i]->data = value;
-
-        if( left != -1 ){
+        if( left > n-1 || right > n-1 ){
+            cout << "Invelid Index " << endl;
+            break;
+        }
+        if (left != -1)
+        {
             allNodes[i]->leftChild = allNodes[left];
         }
 
-        if( right != -1 ){
+        if (right != -1)
+        {
             allNodes[i]->rightChild = allNodes[right];
         }
     }
 
-    printTree( allNodes[0], 0 );
+    printTree(allNodes[0], 0);
     return 0;
 }
 
-/* 
+/*
 
 9
 0 1 2
